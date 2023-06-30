8cc07b26f42169d3290b8ae07d1d7bb8366a2fe6
Gustavo de Jesus <gustavo_carneiro20@yahoo.com.br>
2023-06-30 05:24:17 -0300

    Setting backend structure

---
+++ NodeJs/NLWSpacetime/server/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', () => {})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localserver:3333')
+  })
